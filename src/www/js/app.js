18182e04d57816c6f2dd8df813d3fb1dde0bed60
Eric W. Greene <eric@ericwgreene.com>
2016-10-03 12:18:59 -0500

    day 1 morning code demos

--- src/www/js/app.js
+++ src/www/js/app.js
@@ -1,12 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-export class HelloWorld extends React.Component {
+class ListOfColors extends React.Component {
 
-	render() {
-		return <h1>Hello World!!!</h1>;
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			newColor: ''
+		};
+
+		this.onChange = this.onChange.bind(this);
 	}
 
+	onChange(e) {
+		this.setState({
+			[e.target.name]: e.target.value
+		});
+	}
+
+	render() {
+
+		return <div>
+			<h1>List of Colors</h1>
+			<ul>
+				{this.props.colors.map(color => <li key={color}>{color}</li>)}
+			</ul>
+
+			<div>
+				<label htmlFor="new-color">New Color:</label>
+				<input type="text" id="new-color" name="newColor"
+					value={this.state.newColor} onChange={this.onChange} />
+			</div>
+
+		</div>;
+	}
 }
 
-ReactDOM.render(<HelloWorld />, document.querySelector('main'));
+ListOfColors.propTypes = {
+	colors: React.PropTypes.array.isRequired
+};
+
+const colors = ['green','white','red','yellow','blue','orange','gold','saffron'];
+
+
+ReactDOM.render(<ListOfColors colors={colors} />, document.querySelector('main'));
